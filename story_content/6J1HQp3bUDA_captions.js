96580be93c91d28f3197b7412b609f102aa2352c
selvynessato <137649394+selvynessato@users.noreply.github.com>
2023-07-10 09:48:59 -0600

    JuegoCosami2

---
+++ story_content/6J1HQp3bUDA_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.62.27443.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.650%0D%0AMuchas%20Gracias%20por%20%0Acompartir%20%0Atu%20opini%C3%B3n.%0D%0APor%20favor%20mira%20la%20respuesta%20correcta!%0D%0A%0D%0A"}')
